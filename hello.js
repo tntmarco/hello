9f1fd514aa5096d6bf67dfb5e13588b2b02341b9
tntmarco <marcosouza@outlook.com.br>
2023-06-17 00:06:36 -0300

    teste

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
+  res.end('Hello, World! 22222');
 });
 
 server.listen(3000, 'localhost', () => {
